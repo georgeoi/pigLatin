1d8819ea9b6706b18c222e21e463c837ae223918
georgeoi <--list>
2021-01-21 17:55:43 -0300

    resuelto

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 const PigLatin = function(str){
-    /* YOUR CODE HERE */
+    strArray= str.split("")
     let result
+    newString=[]
+    let num=0
+    let lastCons= 0
+
+    if(strArray[0]=="a"||strArray[0]=="e"||strArray[0]=="i"||strArray[0]=="o"||strArray[0]=="u"){
+       strArray.push(["way"]) 
+
+       result = strArray.join("")
+
+    } else{
+        for (i=1; i<strArray.length; i++){
+            if(strArray[i]=="a"||strArray[i]=="e"||strArray[i]=="i"||strArray[i]=="o"||strArray[i]=="u"){
+            
+            lastCons = (i)
+            break;
+            }      
+            
+        }
+        for (i=lastCons; i<strArray.length;i++){
+            newString.push(strArray[i])
+        }
+        for (i=0;i<lastCons;i++){
+            newString.push(strArray[i])
+        }
+        
+        newString.push(["ay"])
+
+        result = newString.join("")
+    }
+    
+
+    
 
     return result
 }
